a1458ac83c1a39d60cdb5b4667b36c59b18481ae
aaronChang <chuwork.99@gmail.com>
2024-09-14 15:56:13 +0800

    feat: add grid css

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,128 @@
 <script setup></script>
 
 <template>
-  <p>修改了一行</p>
-  <p>新增功能1</p>
+  <div class="d-grid">
+    <div class="item">
+      <img src="https://picsum.photos/300/200/?random=10" />
+      <div>
+        <p class="text">Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="https://picsum.photos/300/200/?random=11" />
+      <div>
+        <p class="text">Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="https://picsum.photos/300/200/?random=12" />
+      <div>
+        <p class="text">Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="https://picsum.photos/300/200/?random=13" />
+      <div>
+        <p class="text">Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="https://picsum.photos/300/200/?random=14" />
+      <div>
+        <p class="text">Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="https://picsum.photos/300/200/?random=15" />
+      <div>
+        <p class="text">Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="https://picsum.photos/300/200/?random=15" />
+      <div>
+        <p class="text">Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="https://picsum.photos/300/200/?random=15" />
+      <div>
+        <p class="text">Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="https://picsum.photos/300/200/?random=15" />
+      <div>
+        <p class="text">Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="https://picsum.photos/300/200/?random=15" />
+      <div>
+        <p class="text">Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="https://picsum.photos/300/200/?random=15" />
+      <div>
+        <p class="text">Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+  </div>
 </template>
 
+<style>
+body {
+  background: #302e36;
+}
+</style>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.d-grid {
+  padding: 20px;
+  max-width: 1200px;
+  margin: 0 auto;
+  display: grid;
+  /* column-gap: 12px; */
+  /* row-gap: 12px; */
+  gap: 12px;
+  /* grid-template-columns: 200px 200px 200px 200px; */
+  grid-template-columns: repeat(4, 1fr);
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+@media screen and (max-width: 972px) {
+  .d-grid {
+    padding: 8px;
+    grid-template-columns: repeat(3, 1fr);
+  }
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+@media screen and (max-width: 576px) {
+  .d-grid {
+    padding: 8px;
+    grid-template-columns: repeat(2, 1fr);
   }
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.item {
+  /* height: 200px; */
+  border: 1px solid #fff;
+  background: #67607a;
+  border-radius: 8px;
+  box-shadow:
+    0 0px 4px 2px rgba(255, 255, 255, 0.15),
+    0 0px 4px 2px rgba(255, 255, 255, 0.15);
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  padding: 20px;
+  gap: 12px;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+/* item > src內有picsum */
+.item > img[src*='picsum'] {
+  width: 100px;
+  aspect-ratio: 1/1;
+  object-fit: cover;
 }
 </style>
